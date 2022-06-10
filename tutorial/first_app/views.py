d005684d64f88239e89845fa1dd9b7e47e5001f3
Sergiusz S <sergiusz.styczen@gmail.com>
2022-06-10 15:46:33 +0200

    First page routing

--- tutorial/first_app/views.py
+++ tutorial/first_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'page_title': 'First Django App'
+    }
+    return render(request, 'index.html', context)
